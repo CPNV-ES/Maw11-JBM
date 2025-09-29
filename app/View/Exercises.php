ac3a5d63519fe411743c2177224dcff345c302df
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-29 11:15:33 +0200

    feat: Create multiple dummy exercises

--- app/View/Exercises.php
+++ app/View/Exercises.php
@@ -1,1 +1,34 @@
-<?php
+<div class="column-card">
+    <div class="title">Bash</div>
+    <a class="button" href="">Take it</a>
+</div>
+
+<div class="column-card">
+    <div class="title">Python</div>
+    <a class="button" href="">Take it</a>
+</div>
+
+<div class="column-card">
+    <div class="title">Javascript</div>
+    <a class="button" href="">Take it</a>
+</div>
+
+<div class="column-card">
+    <div class="title">PHP</div>
+    <a class="button" href="">Take it</a>
+</div>
+
+<div class="column-card">
+    <div class="title">GIT</div>
+    <a class="button" href="">Take it</a>
+</div>
+
+<div class="column-card">
+    <div class="title">C++</div>
+    <a class="button" href="">Take it</a>
+</div>
+
+<div class="column-card">
+    <div class="title">C#</div>
+    <a class="button" href="">Take it</a>
+</div>
