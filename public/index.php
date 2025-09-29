a0b217150ef739d20a6b3b353f712d1b320d8589
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-29 18:28:46 +0200

    feat: Apply body to index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
 <?php
+define('BASE_DIR', dirname( __FILE__ ).'/..');
+define('SOURCE_DIR', BASE_DIR.'/app');
+
+include SOURCE_DIR.'/View/Exercises.php'; ?>
+<link rel="stylesheet" href="/css/Body.css">
+
