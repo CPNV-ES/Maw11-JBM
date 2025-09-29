957a3232c7e35d3b596db3dec2407f2a181f5d24
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-29 11:21:40 +0200

    feat: Wrap content with <ul> and <li>

--- app/View/Exercises.php
+++ app/View/Exercises.php
@@ -1,34 +1,49 @@
-<div class="column-card">
-    <div class="title">Bash</div>
-    <a class="button" href="">Take it</a>
-</div>
+<ul>
+    <li>
+        <div class="column card">
+            <div class="title">Bash</div>
+            <a class="button" href="">Take it</a>
+        </div>
+    </li>
 
-<div class="column-card">
-    <div class="title">Python</div>
-    <a class="button" href="">Take it</a>
-</div>
+    <li>
+        <div class="column card">
+            <div class="title">Python</div>
+            <a class="button" href="">Take it</a>
+        </div>
+    </li>
+    <li>
+        <div class="column card">
+            <div class="title">Javascript</div>
+            <a class="button" href="">Take it</a>
+        </div>
+    </li>
 
-<div class="column-card">
-    <div class="title">Javascript</div>
-    <a class="button" href="">Take it</a>
-</div>
+    <li>
+        <div class="column card">
+            <div class="title">PHP</div>
+            <a class="button" href="">Take it</a>
+        </div>
+    </li>
 
-<div class="column-card">
-    <div class="title">PHP</div>
-    <a class="button" href="">Take it</a>
-</div>
+    <li>
+        <div class="column card">
+            <div class="title">GIT</div>
+            <a class="button" href="">Take it</a>
+        </div>
+    </li>
 
-<div class="column-card">
-    <div class="title">GIT</div>
-    <a class="button" href="">Take it</a>
-</div>
+    <li>
+        <div class="column card">
+            <div class="title">C++</div>
+            <a class="button" href="">Take it</a>
+        </div>
 
-<div class="column-card">
-    <div class="title">C++</div>
-    <a class="button" href="">Take it</a>
-</div>
-
-<div class="column-card">
-    <div class="title">C#</div>
-    <a class="button" href="">Take it</a>
-</div>
+    </li>
+    <li>
+        <div class="column card">
+            <div class="title">C#</div>
+            <a class="button" href="">Take it</a>
+        </div>
+    </li>
+</ul>
