e926f63bf0c2e18e01c9b4e8c1e3ce093ee71c8b
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-30 21:40:12 +0200

    feat : Create routes for exercises, exercise and home page

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,14 @@
 <?php
 
 use core\Router;
+use Maw11Jbm\Controllers\ExercisesController;
+use Maw11Jbm\Controllers\HomePageController;
+use Maw11Jbm\Controllers\ExerciseController;
 use Maw11Jbm\Controllers\UserController;
 
 /** @var Router $router */
 
-$router->addRoute('GET', '/users', [UserController::class, 'index']);
+$router->addRoute('GET', '/', [HomePageController::class, 'index']);
 
-$router->addRoute('GET', '/user/{id}', [UserController::class, 'show']);
+$router->addRoute('GET', '/exercises', [ExercisesController::class, 'showAllExercises']);
+$router->addRoute('GET', '/exercises/{id}', [ExerciseController::class, 'show']);
