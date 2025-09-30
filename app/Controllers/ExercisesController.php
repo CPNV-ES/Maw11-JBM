f0dc6ad32c393f8248549a892eb839048748e32c
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-30 21:15:16 +0200

    feat : Create ExerciesController

---
+++ app/Controllers/ExercisesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Maw11Jbm\Controllers;
+
+use core\view;
+
+class ExercisesController
+{
+    public function showAllExercises(): false|string
+    {
+       return new View()->view('Exercises.php');
+    }
+
+}
