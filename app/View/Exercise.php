fca7257a4fd47d47e801af6e9f790eda8b5533a0
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-30 11:14:52 +0200

    feat : Create the exercise view

--- app/View/Exercise.php
+++ app/View/Exercise.php
@@ -1,7 +1,23 @@
-<?php
+<div class="container">
 
-namespace Maw11Jbm\View;
+<h1>Your take</h1>
+<p>If you'd like to come back later to finish, simply submit it with blanks</p>
 
-class Exercise
-{
-}
+<form action="/exercises/58/fulfillments" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="F8qtp2lkUvqU7r736LLbLRt9cM4dyyr3unmzbILmAnBfqNW3FmU6b1FAzndjOus7roWiF4lMyIHr6g1KSaekOA==">
+  
+    
+      
+      <input type="hidden" value="190" name="fulfillment[answers_attributes][][field_id]" id="fulfillment_answers_attributes__field_id">
+      <div class="field">
+        <label for="fulfillment_answers_attributes__value">TEST</label>
+<input type="text" name="fulfillment[answers_attributes][][value]" id="fulfillment_answers_attributes__value">
+
+      </div>
+    
+  <div class="actions">
+    <input type="submit" name="commit" value="Save" data-disable-with="Save">
+  </div>
+</form>
+
+
+</div>
