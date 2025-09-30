ea19528c576a24f089c6caaea8dbcb93fd9a0cff
bheutschi <bheutschi@gmail.com>
2025-09-30 12:34:57 +0200

    feat(router): add the router in the index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
 <?php
+
+use core\Router;
+
+const BASE_DIR = __DIR__ . '/..';
+const APP_DIR  = BASE_DIR . '/app';
+
+require BASE_DIR . '/vendor/autoload.php';
+
+$router = new Router();
+
+include APP_DIR . '/routes.php';
+
+try {
+    $router->resolve();
+} catch (Exception $e) {
+    http_response_code(500);
+
+    return 'Internal Server Error: ' . $e->getMessage();
+}
