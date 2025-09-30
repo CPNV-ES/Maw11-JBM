ca25c77e995b64a003a8f7902420656e0f8ec820
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-30 21:41:11 +0200

    feat : Create function show

--- app/Controllers/ExerciseController.php
+++ app/Controllers/ExerciseController.php
@@ -2,6 +2,17 @@
 
 namespace Maw11Jbm\Controllers;
 
+use core\View;
+
 class ExerciseController
 {
+    public function show(array $params): false|string
+    {
+        $exerciseId = filter_var($params['id'], FILTER_VALIDATE_INT);
+        if ($exerciseId === false) {
+            return 'Invalid exercise ID';
+        }
+
+        return new View()->view('Exercise.php');
+    }
 }
