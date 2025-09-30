7994e73ff40b082ffa0eb32dc4b487b878f72d4e
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-30 10:21:12 +0200

    refactor: Altering hard coded html in a loop

--- app/View/Exercises.php
+++ app/View/Exercises.php
@@ -1,50 +1,11 @@
 <link rel="stylesheet" href="/css/Exercises.css">
 <ul>
+    <?php foreach (['Bash', 'Python', 'Javascript','PHP','GIT','C++','C#'] as $title): ?>
     <li>
         <div class="column card">
-            <div class="title">Bash</div>
+            <div class="title"><?= $title ?></div>
             <a class="button" href="">Take it</a>
         </div>
     </li>
-
-    <li>
-        <div class="column card">
-            <div class="title">Python</div>
-            <a class="button" href="">Take it</a>
-        </div>
-    </li>
-    <li>
-        <div class="column card">
-            <div class="title">Javascript</div>
-            <a class="button" href="">Take it</a>
-        </div>
-    </li>
-
-    <li>
-        <div class="column card">
-            <div class="title">PHP</div>
-            <a class="button" href="">Take it</a>
-        </div>
-    </li>
-
-    <li>
-        <div class="column card">
-            <div class="title">GIT</div>
-            <a class="button" href="">Take it</a>
-        </div>
-    </li>
-
-    <li>
-        <div class="column card">
-            <div class="title">C++</div>
-            <a class="button" href="">Take it</a>
-        </div>
-
-    </li>
-    <li>
-        <div class="column card">
-            <div class="title">C#</div>
-            <a class="button" href="">Take it</a>
-        </div>
-    </li>
+    <?php endforeach; ?>
 </ul>
