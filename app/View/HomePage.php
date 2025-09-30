1d450dbad19d1b96c4cef6f9c71091b6e168b728
Jorisbly <joris.bailly.2001@gmail.com>
2025-09-30 21:41:40 +0200

    feat : Add an onclick event to go in exercises route

--- app/View/HomePage.php
+++ app/View/HomePage.php
@@ -7,7 +7,7 @@
     
     <main class="content">
         <div class="buttons-container">
-            <button class="button purple">TAKE AN EXERCISE</button>
+            <button onclick="location.href='exercises'" class="button purple">TAKE AN EXERCISE</button>
             <button class="button orange">CREATE AN EXERCISE</button>
             <button class="button green">MANAGE AN EXERCISE</button>
         </div>
