64cd45429f8d5dfaf348aefe9e0d82c194993307
Jorisbly <joris.bailly.2001@gmail.com>
2025-10-28 11:14:45 +0100

    refactor : Update function to work with new version of Database class

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -2,6 +2,22 @@
 
 namespace Maw11Jbm\Models;
 
+use core\Database;
+
 class Exercise
 {
+    private string $id;
+    private string $title;
+    private string $status;
+
+    public function __construct($title, $status)
+    {
+        $this->$title = $title;
+        $this->$status = $status;
+    }
+
+    function getExercises(){
+        $results = Database::getInstance()->getAll('exercises');
+        return $results->fetchAll();
+    }
 }
