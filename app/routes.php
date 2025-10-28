df4da8fb065bc2d086e0b300e6d348290ca1dbf8
Jorisbly <joris.bailly.2001@gmail.com>
2025-10-28 11:17:28 +0100

    feat(): Add route for list of exercises

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,5 @@ use Maw11Jbm\Controllers\HomePageController;
 /** @var Router $router */
 
 $router->addRoute('GET', '/', [HomePageController::class, 'index']);
-
 $router->addRoute('GET', '/exercises', [ExercisesController::class, 'showAllExercises']);
 $router->addRoute('GET', '/exercises/{id}', [ExerciseController::class, 'show']);
