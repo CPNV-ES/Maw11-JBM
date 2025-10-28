0e522148c5664dd73098fdeb473312ebd7499505
bheutschi <bheutschi@gmail.com>
2025-10-28 10:42:52 +0100

    feat: add the index methode

--- app/Controllers/ExerciseController.php
+++ app/Controllers/ExerciseController.php
@@ -2,10 +2,17 @@
 
 namespace Maw11Jbm\Controllers;
 
+use Maw11Jbm\Models\Exercise;
 use function core\view;
 
 class ExerciseController
 {
+
+    public function index(): false|string
+    {
+        return view('exercises/index.php', ['buildings' => Exercise::building(), 'closed' => Exercise::closed(), 'answerings' => Exercise::answering()]);
+    }
+    
     public function show(array $params): false|string
     {
         $exerciseId = filter_var($params['id'], FILTER_VALIDATE_INT);
@@ -13,6 +20,6 @@ class ExerciseController
             return 'Invalid exercise ID';
         }
 
-        return view('exercise.php');
+        return view('show.php');
     }
 }
