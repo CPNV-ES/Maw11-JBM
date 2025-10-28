e1447cc62605495c559ae78eb4078f87e26fc1b0
bheutschi <bheutschi@gmail.com>
2025-10-28 10:45:51 +0100

    refactor: refactor with php fixer

--- app/Controllers/ExerciseController.php
+++ app/Controllers/ExerciseController.php
@@ -2,17 +2,17 @@
 
 namespace Maw11Jbm\Controllers;
 
-use Maw11Jbm\Models\Exercise;
 use function core\view;
+
+use Maw11Jbm\Models\Exercise;
 
 class ExerciseController
 {
-
     public function index(): false|string
     {
         return view('exercises/index.php', ['buildings' => Exercise::building(), 'closed' => Exercise::closed(), 'answerings' => Exercise::answering()]);
     }
-    
+
     public function show(array $params): false|string
     {
         $exerciseId = filter_var($params['id'], FILTER_VALIDATE_INT);
--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -17,25 +17,28 @@ class Exercise
             ['id' => 8, 'title' => 'Exercise 8'],
             ['id' => 9, 'title' => 'Exercise 9'],
             ['id' => 10, 'title' => 'Exercise 10'],
-            ['id' => 11, 'title' => 'Exercise 11']
+            ['id' => 11, 'title' => 'Exercise 11'],
         ];
     }
+
     public static function building(): array
     {
         return [
             ['id' => 1, 'title' => 'Exercise 1'],
             ['id' => 2, 'title' => 'Exercise 2'],
-            ['id' => 3, 'title' => 'Exercise 3']
+            ['id' => 3, 'title' => 'Exercise 3'],
         ];
     }
+
     public static function answering(): array
     {
         return [
             ['id' => 4, 'title' => 'Exercise 4'],
             ['id' => 5, 'title' => 'Exercise 5'],
-            ['id' => 6, 'title' => 'Exercise 6']
+            ['id' => 6, 'title' => 'Exercise 6'],
         ];
     }
+
     public static function closed(): array
     {
         return [
@@ -43,7 +46,7 @@ class Exercise
             ['id' => 8, 'title' => 'Exercise 8'],
             ['id' => 9, 'title' => 'Exercise 9'],
             ['id' => 10, 'title' => 'Exercise 10'],
-            ['id' => 11, 'title' => 'Exercise 11']
+            ['id' => 11, 'title' => 'Exercise 11'],
         ];
     }
 }
