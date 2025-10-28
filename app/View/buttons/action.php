c2cc06cb716039eff9d87c7117358723dbe90ec0
Dragecko <dragecko@etik.com>
2025-10-28 21:49:45 +0100

    feat: Adding the first button

--- app/View/buttons/action.php
+++ app/View/buttons/action.php
@@ -1,5 +1,24 @@
 <link rel="stylesheet" href="/css/buttons.css">
+<?php
 
+// default value if not set
+$icon = $icon ?? 'close';
+$color = $color ?? 'primary';
+
+// dinamic css classes
+$colorClass = "btn-{$color}";
+
+?>
 <button type="button" 
-    class="btn btn-default btn-small 
+    class="btn btn-default btn-small"
+        <?= $colorClass ?>
+    >
+
+    <?php if ($icon === 'close'): ?>
+        <!-- Close icon -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+
+    <?php endif; ?>
 </button>
