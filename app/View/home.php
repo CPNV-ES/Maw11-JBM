1327d0e3ec89d27c5a4c0f922b9f7399bd6ff474
bheutschi <bheutschi@gmail.com>
2025-10-29 09:23:21 +0100

    feat: add the button to go on create exercise page

--- app/View/home.php
+++ app/View/home.php
@@ -8,7 +8,7 @@
     <main class="content">
         <div class="buttons-container">
             <button onclick="location.href='/exercises/answering'" class="button purple">TAKE AN EXERCISE</button>
-            <button class="button orange">CREATE AN EXERCISE</button>
+            <button onclick="location.href='exercises/new'" class="button orange">CREATE AN EXERCISE</button>
             <button onclick="location.href='exercises'" class="button green">MANAGE AN EXERCISE</button>
         </div>
     </main>
