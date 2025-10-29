a85505005bb8465d2ef02f8bf8a233256206dc82
bheutschi <bheutschi@gmail.com>
2025-10-29 09:25:46 +0100

    feat: add the route to display and create exercise
    # Conflicts:
    #	app/routes.php

--- app/routes.php
+++ app/routes.php
@@ -9,5 +9,7 @@ use Maw11Jbm\Controllers\HomeController;
 $router->addRoute('GET', '/', [HomeController::class, 'index']);
 $router->addRoute('GET', '/exercises', [ExerciseController::class, 'index']);
 $router->addRoute('GET', '/exercises/answering', [ExerciseController::class, 'indexAnswering']);
+$router->addRoute('GET', '/exercises/new', [ExerciseController::class, 'create']);
+$router->addRoute('POST', '/exercises', [ExerciseController::class, 'store']);
 $router->addRoute('GET', '/exercises/{id}', [ExerciseController::class, 'show']);
 $router->addRoute('POST', '/exercises/{id}', [ExerciseController::class, 'delete']);
