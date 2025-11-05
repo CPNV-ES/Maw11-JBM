27914cb06b05ccf98ed2fdc899a5fa2221ef868b
jorisbly <joris.bailly.2001@gmail.com>
2025-11-05 12:20:39 +0100

    feat(Exercise): return exercises by specific state

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -40,30 +40,16 @@ class Exercise
 
     public static function building(): array
     {
-        return [
-            ['id' => 1, 'title' => 'Exercise 1'],
-            ['id' => 2, 'title' => 'Exercise 2'],
-            ['id' => 3, 'title' => 'Exercise 3'],
-        ];
+        return Database::getInstance()->getAll('exercises', 'status', 'building');
     }
 
     public static function answering(): array
     {
-        return [
-            ['id' => 4, 'title' => 'Exercise 4'],
-            ['id' => 5, 'title' => 'Exercise 5'],
-            ['id' => 6, 'title' => 'Exercise 6'],
-        ];
+        return Database::getInstance()->getAll('exercises', 'status', 'answering');
     }
 
     public static function closed(): array
     {
-        return [
-            ['id' => 7, 'title' => 'Exercise 7'],
-            ['id' => 8, 'title' => 'Exercise 8'],
-            ['id' => 9, 'title' => 'Exercise 9'],
-            ['id' => 10, 'title' => 'Exercise 10'],
-            ['id' => 11, 'title' => 'Exercise 11'],
-        ];
+        return Database::getInstance()->getAll('exercises', 'status', 'closed');
     }
 }
