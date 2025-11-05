79ec09ab156b8e6a5abacb96fc8db4d6e9712100
bheutschi <bheutschi@gmail.com>
2025-11-05 13:32:02 +0100

    feat(exercise): add the form in the view

--- app/View/exercises/create.php
+++ app/View/exercises/create.php
@@ -6,14 +6,12 @@
         <a href="/"><img src="/assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png"></a>
     </section>
 </header>
-<div class="container main row">
+<div class="container main">
     <h1>New Exercise</h1>
-    <form action="/exercises" accept-charset="UTF-8" method="post">
-        <input name="utf8" type="hidden" value="&#x2713;"/>
-        <input type="hidden" name="authenticity_token" value="vud/TP9beMUr9R6ct/0MvJweI1o3M5UYHgmLOUrxTGw/YAjVo21ftD8ynxTsoNjSA1JwQ3Wac1FEegsGV8QrnQ=="/>
+    <form action="/exercises" accept-charset="UTF-8" method="post" enctype="multipart/form-data">
         <div class="field">
             <label for="exercise_title">Title</label>
-            <input type="text" name="exercise[title]" id="exercise_title"/>
+            <input type="text" name="exercise_title" id="exercise_title" required/>
         </div>
         <div class="actions">
             <input type="submit" name="commit" value="Create Exercise" data-disable-with="Create Exercise"/>
