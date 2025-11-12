f4b8fa11564bdbf5aec37414dbd0ebc106ad3939
bheutschi <bheutschi@gmail.com>
2025-11-12 10:56:19 +0100

    feat: add FieldController for managing fields and update routes for field creation

---
+++ app/Controllers/FieldController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Maw11Jbm\Controllers;
+
+use Maw11Jbm\Models\Exercise;
+use Maw11Jbm\Models\Field;
+use function core\view;
+
+class FieldController
+{
+    public function store(): false|string
+    {
+        if (!empty($_POST["field_label"])) {
+            Field::create([
+                'label' => $_POST["field_label"],
+                'value_kind' => $_POST["field_value_kind"],
+                'exercises_id' => $_POST["exercise_id"]
+            ]);
+            header("Location: " . $_SERVER['REQUEST_URI']);
+            exit;
+        }
+        return view('exercises/create.php');
+    }
+}
