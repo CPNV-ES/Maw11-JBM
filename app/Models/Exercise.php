03560ba12e57d35ddc5b35e62a25f4762b73df45
jorisbly <joris.bailly.2001@gmail.com>
2025-11-12 12:20:07 +0100

    feat(): Create function edit in Exercise Model

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -16,27 +16,11 @@ class Exercise
         $this->$status = $status;
     }
 
-    static function getExercises() :array
+    static function all() :array
     {
         return Database::getInstance()->getAll('exercises');
     }
 
-    public static function all(): array
-    {
-        return [
-            ['id' => 1, 'title' => 'Exercise 1'],
-            ['id' => 2, 'title' => 'Exercise 2'],
-            ['id' => 3, 'title' => 'Exercise 3'],
-            ['id' => 4, 'title' => 'Exercise 4'],
-            ['id' => 5, 'title' => 'Exercise 5'],
-            ['id' => 6, 'title' => 'Exercise 6'],
-            ['id' => 7, 'title' => 'Exercise 7'],
-            ['id' => 8, 'title' => 'Exercise 8'],
-            ['id' => 9, 'title' => 'Exercise 9'],
-            ['id' => 10, 'title' => 'Exercise 10'],
-            ['id' => 11, 'title' => 'Exercise 11'],
-        ];
-    }
 
     public static function building(): array
     {
@@ -51,6 +35,11 @@ class Exercise
     public static function closed(): array
     {
         return Database::getInstance()->getAll('exercises', 'status', 'closed');
+    }
+
+    public static function edit($id, $POST): void
+    {
+        Database::getInstance()->editItem('exercises',  $POST, $id);
     }
     public static function delete($id): void
     {
