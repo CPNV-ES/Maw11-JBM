d76bb9186b37a83e01f09ff8ef4c14d47b34379d
Dragecko <98613796+Dragecko@users.noreply.github.com>
2025-11-12 14:21:59 +0100

    Feature/btn normalise (#43)
    * feat: new button system applied on home.php
    
    * feat: brain of the button fonction, basicaly juste a normalised button (work the same way as action.php)
    
    * fix: now the button style is moved to buttons-navigation.css
    
    * feat: this is the new CSS for the button, its a modified version of the button of home.php
    
    * fix: named button fixed
    
    * fix: useless function in the button navigation.php
    
    * fix: follow graphic chart
    
    * fix: now the new button systems work with anything and anywhere

--- app/View/exercises/index_answering.php
+++ app/View/exercises/index_answering.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="/css/exercises.css">
 <link rel="stylesheet" href="/css/index.css">
+
 <header class="heading answering">
     <section class="container">
         <a href="/"><img src="/assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png"></a>
@@ -12,8 +13,14 @@
             <?php foreach ($data as $exercise): ?>
                 <li>
                     <div class="column card">
-                        <div class="title"><?= $exercise['TITLE'] ?></div>
-                        <a class="button" href=<?= $exercise['ID'] ?>>Take it</a>
+                        <div class="title"><?= $exercise['title'] ?></div>
+                        <?php
+                            $href = '/exercises/' . $exercise['id'];
+                            $color = 'purple';
+                            $label = 'Take it';
+                            $icon = null;
+                            include __DIR__ . '/../../../core/buttons/navigation.php';
+                        ?>
                     </div>
                 </li>
             <?php endforeach; ?>
