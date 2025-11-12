bd5b63074e8363cc6f969648ddade88b28a9c02e
bheutschi <bheutschi@gmail.com>
2025-11-12 10:55:42 +0100

    feat: update showAllExercises method to use all() for fetching exercises

--- app/Controllers/ExercisesController.php
+++ app/Controllers/ExercisesController.php
@@ -2,16 +2,14 @@
 
 namespace Maw11Jbm\Controllers;
 
-use function core\view;
-
 use Maw11Jbm\Models\Exercise;
+use function core\view;
 
 class ExercisesController
 {
     public function showAllExercises(): false|string
     {
-        $data = Exercise::getExercises();
-
+        $data = Exercise::all();
         return view('exercises.php', $data);
     }
 }
