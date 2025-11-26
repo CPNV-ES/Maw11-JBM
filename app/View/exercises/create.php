3ed8aefc545384aeba94b3924a3c3742de641667
bheutschi <bheutschi@gmail.com>
2025-11-26 12:43:31 +0100

    feat: create new exercise page with form for exercise submission

--- app/View/exercises/create.php
+++ app/View/exercises/create.php
@@ -1,11 +1,4 @@
-<header class="heading answering">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="/css/exercises.css">
-    <link rel="stylesheet" href="/css/index.css">
-    <section class="container">
-        <a href="/"><img src="/assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png"></a>
-    </section>
-</header>
+<?php $title = 'New Exercise' ?>
 <div class="container main">
     <h1>New Exercise</h1>
     <form action="/exercises" accept-charset="UTF-8" method="post" enctype="multipart/form-data">
