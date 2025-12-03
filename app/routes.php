ec691a7bcf7bc1896124cd9fdfeecefae205a646
bheutschi <bheutschi@gmail.com>
2025-12-03 12:07:45 +0100

    feat: add edit and update routes for fields in FieldController

--- app/routes.php
+++ app/routes.php
@@ -15,5 +15,7 @@ $router->addRoute('POST', '/exercises', [ExerciseController::class, 'store']);
 $router->addRoute('GET', '/exercises/{id}', [ExerciseController::class, 'show']);
 $router->addRoute('PUT', '/exercises/{id}', [ExerciseController::class, 'update']);
 $router->addRoute('GET', '/exercises/{id}/fields', [ExerciseController::class, 'edit']);
+$router->addRoute('GET', '/exercises/{exerciseId}/fields/{fieldId}/edit', [FieldController::class, 'edit']);
+$router->addRoute('PATCH', '/exercises/{exerciseId}/fields/{fieldId}/edit', [FieldController::class, 'update']);
 $router->addRoute('POST', '/exercises/{id}/fields', [FieldController::class, 'store']);
 $router->addRoute('POST', '/exercises/{id}', [ExerciseController::class, 'delete']);
