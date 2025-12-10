f9e8a8bd06346728d4837dcb73089483b42093da
Jorisbly <joris.bailly.2001@gmail.com>
2025-12-10 11:36:36 +0100

    feat(): add exercise_id and change input with resukt to answer

--- app/View/results/create.php
+++ app/View/results/create.php
@@ -3,10 +3,11 @@
     <h1>Your take</h1>
     <p>If you'd like to come back later to finish, simply submit it with blanks</p>
     <form action="<?= '/exercises/' . $exercise['id']. '/results' ?>" accept-charset="UTF-8" method="post">
+        <input type="hidden" name="exercises_id" value="<?= $exercise['id'] ?>">
         <?php foreach ($exercise['fields'] as $field): ?>
         <div class="field">
-            <label for="result"><?= $field['label'] ?></label>
-            <input type="text" name="result_<?=$field['id']?>" id="result">
+            <label for="answer_<?=$field['id']?>"><?= $field['label'] ?></label>
+            <input type="text" name="answer_<?=$field['id']?>" id="answer_<?=$field['id']?>">
         </div>
         <?php endforeach; ?>
         <div class="actions">
