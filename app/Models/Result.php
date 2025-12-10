1e96b712a79b68ea4cb1ca4fb42badfef8932ad3
Jorisbly <joris.bailly.2001@gmail.com>
2025-12-10 14:47:24 +0100

    feat(): Get a result with his fulfillments

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -3,6 +3,7 @@
 namespace Maw11Jbm\Models;
 
 use core\Database;
+use Illuminate\Support\Collection;
 
 class Result
 {
@@ -20,4 +21,40 @@ class Result
     {
         return Database::getInstance()->createItem('results', $item);
     }
+
+    public static function allWithFulfillments(int $id): array
+    {
+        $rows = Database::getInstance()->fetchAllWithJoins(
+            'results r',
+            [
+                [
+                    'type'  => 'LEFT',
+                    'table' => 'fulfillments f',
+                    'on'    => 'f.results_id = r.id',
+                ],
+            ],
+            'r.id = :id',
+            ['id' => $id],
+            'r.id AS result_id, f.id AS fulfillment_id, f.answer,f.fields_id, f.created_at'
+        );
+        return self::mapResultWithFulfillments($rows);
+    }
+    public static function mapResultWithFulfillments(Collection|array $rows): array
+    {
+        $rows = collect($rows);
+
+        return $rows->isEmpty()
+            ? []
+            : [
+                'id'     => $rows->first()['result_id'],
+                'fulfillments' => $rows->whereNotNull('fulfillment_id')
+                    ->map(fn ($r) => [
+                        'id'         => $r['fulfillment_id'],
+                        'answer'      => $r['answer'],
+                        'created_at' => $r['created_at'],
+                        'fields_id' => $r['fields_id'],
+                    ])->values()->all(),
+            ];
+    }
+
 }
