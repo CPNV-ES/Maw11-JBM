be59c69ceabf8ef0bc8d5c48fa40648384579b65
Jorisbly <joris.bailly.2001@gmail.com>
2025-12-10 10:58:09 +0100

    refactor(): Rename file fulfillement with an uppercase f

---
+++ app/Models/Fulfillement.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Maw11Jbm\Models;
+
+use core\Database;
+
+class fulfillment
+{
+    public static function all(): array
+    {
+        return Database::getInstance()->getAll('fulfillments');
+    }
+
+    public static function create(array $item): int
+    {
+        return Database::getInstance()->createItem('fulfillments', $item);
+    }
+}
