caae6b7e4d83b9f9b9dd798d239c10abde9e57ba
bheutschi <bheutschi@gmail.com>
2025-12-16 10:28:27 +0100

    feat: enhance error handling in router and index with custom error views

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
-
+namespace core;
 use core\Database;
 use core\Router;
+use Throwable;
 
 const BASE_DIR = __DIR__ . '/..';
 const APP_DIR  = BASE_DIR . '/app';
@@ -15,9 +16,9 @@ $router = new Router();
 include APP_DIR . '/routes.php';
 
 try {
-    $router->resolve();
-} catch (Exception $e) {
+    $output = $router->resolve();
+    echo $output;
+} catch (Throwable $e) {
     http_response_code(500);
-
-    return 'Internal Server Error: ' . $e->getMessage();
+    return view('errors/500.php', ['message' => $e->getMessage()]);
 }
