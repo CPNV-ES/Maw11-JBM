20e488ef4baff853922c20d0655441028cf0334b
Dragecko <dragecko@etik.com>
2025-12-17 11:55:10 +0100

    feat: vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        app: 'resources/css/app.css',
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'css/app.css'
+          }
+          return 'assets/[name]-[hash][extname]'
+        },
+      },
+    },
+  },
 })
