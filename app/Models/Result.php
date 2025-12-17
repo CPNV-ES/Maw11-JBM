6b02875bc3c928335841e518ebc206eabcef31fe
Jorisbly <joris.bailly.2001@gmail.com>
2025-12-17 12:25:38 +0100

    feat(): Request reuslts for a specific exercise

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -16,12 +16,86 @@ class Result
     {
         return Database::getInstance()->findById('results', $id);
     }
+    public function index(array $params): false|string {
+        $exerciseId = filter_var($params['id']);
+        return view('results/index.php', [
+            'results' => Result::getResultByExerciseId($exerciseId)]);
+    }
 
     public static function create(array $item): int
     {
         return Database::getInstance()->createItem('results', $item);
     }
 
+
+    public static function getResultByExerciseId(int $exerciseId): array
+    {
+        $rows =  Database::getInstance()->fetchAllWithJoins(
+            'exercises e',
+            [
+                [
+                    'type'  => 'LEFT',
+                    'table' => 'fields fi',
+                    'on'    => 'fi.exercises_id = e.id',
+                ],
+                [
+                    'type'  => 'LEFT',
+                    'table' => 'results r',
+                    'on'    => 'r.exercises_id = e.id',
+                ],
+                [
+                    'type'  => 'LEFT',
+                    'table' => 'fulfillments fu',
+                    'on' => 'fu.fields_id = fi.id',
+                    'and' => 'fu.results_id = r.id'
+                ]
+            ],
+            'e.id = :id',
+            ['id' => $exerciseId],
+            'e.id AS exercise_id, e.title, 
+            fi.id AS field_id,fi.label, fi.value_kind, 
+            r.id AS results_id, r.created_at,
+            fu.id AS fulfillment_id, fu.fields_id, fu.answer, fu.created_at,fu.updated_at',
+        );
+
+        return self::mapAllResultsOfExrcise($rows);
+    }
+
+    public static function mapAllResultsOfExrcise(Collection|array $rows): array
+    {
+        $rows = collect($rows);
+        return $rows->isEmpty()
+            ? []
+            : [
+                'id'     => $rows->first()['exercise_id'],
+                'title'  => $rows->first()['title'],
+                'fields' => $rows
+                    ->whereNotNull('field_id')
+                    ->unique('field_id')
+                    ->map(fn ($r) => [
+                        'id'         => $r['field_id'],
+                        'label'      => $r['label'],
+                        'value_kind' => $r['value_kind'],
+                    ])->values()->all(),
+                'results'=> $rows
+                    ->whereNotNull('results_id')
+                    ->unique('results_id')
+                    ->map(fn ($res) => [
+                        'id' => $res['results_id'],
+                        'created_at' => $res['created_at'],
+                        'fulfillments' => $rows
+                            ->whereNotNull('fulfillment_id')
+                            ->where('results_id', $res['results_id'])
+                            ->unique('fulfillment_id')
+                            ->map(fn ($f) => [
+                                'id' => $f['fulfillment_id'],
+                                'answer' => $f['answer'],
+                                'fields_id' => $f['fields_id'],
+                                'created_at' => $f['created_at'],
+                            ])
+                    ]),
+            ];
+    }
     public static function allWithFulfillments(int $id): array
     {
         $rows = Database::getInstance()->fetchAllWithJoins(
@@ -56,5 +130,6 @@ class Result
                     ])->values()->all(),
             ];
     }
+
 
 }
