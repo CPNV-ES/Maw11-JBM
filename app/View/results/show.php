16c395ead0a339617ee7e70717ab0e8c19659b21
bheutschi <bheutschi@gmail.com>
2025-12-23 12:52:31 +0100

    fix: improve layout and styling of exercise results display

--- app/View/results/show.php
+++ app/View/results/show.php
@@ -1,13 +1,11 @@
 <?php
-$title      = 'exercises';
+$title      = 'Exercises';
 $labelTitle = $results['exercise_title'];
 ?>
-<div class="container main">
-    <h1><?= $results['result_date'] ?></h1>
-    <dl>
-        <?php foreach ($results['fulfillments'] as $result): ?>
-        <dt><?= $result['field_label'] ?></dt>
-        <dd><?= $result['answer'] ?></dd>
-        <?php endforeach;?>
-    </dl>
-</div>
+<h1><?= $results['result_date'] ?></h1>
+<dl class="mt-4">
+    <?php foreach ($results['fulfillments'] as $result): ?>
+        <dt class="fw-bold mt-3"><?= htmlspecialchars($result['field_label']) ?></dt>
+        <dd class="ms-3 mt-3 mb-6 text-muted"> <span class="me-2">•</span><?= htmlspecialchars($result['answer']) ?></dd>
+    <?php endforeach; ?>
+</dl>
