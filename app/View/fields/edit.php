c162bfdf2206582a87232cf33dc1f839549a5ff8
bheutschi <bheutschi@gmail.com>
2025-12-23 12:53:54 +0100

    fix: improve form layout and styling for better user experience

--- app/View/fields/edit.php
+++ app/View/fields/edit.php
@@ -2,19 +2,18 @@
 $title      = 'New';
 $labelTitle = $exercise['title'];
 ?>
-<div class="container main">
     <h1>Editing Field</h1>
     <form action="/exercises/<?= $exercise['id'] ?>/fields/<?= $field['id'] ?>/edit" accept-charset="UTF-8" method="POST">
         <input type="hidden" name="_method" value="PATCH">
         <input type="hidden" name="field_id" value="<?= $field['id'] ?>">
         <input type="hidden" name="exercise_id" value="<?= $exercise['id'] ?>">
-        <div class="field">
-            <label for="field_label">Label</label>
-            <input class="input" value="<?= htmlspecialchars($field['label'], ENT_QUOTES, 'UTF-8') ?>" type="text" name="field_label" id="field_label" required/>
+        <div class="mb-3">
+            <label for="field_label" class="form-label">Label</label>
+            <input class="form-control" value="<?= htmlspecialchars($field['label'], ENT_QUOTES, 'UTF-8') ?>" type="text" name="field_label" id="field_label" required/>
         </div>
-        <div class="field">
-            <label for="field_value_kind">Value kind</label>
-            <select name="field_value_kind" id="field_value_kind">
+        <div class="mb-3">
+            <label for="field_value_kind" class="form-label">Value kind</label>
+            <select class="form-select" name="field_value_kind" id="field_value_kind">
                 <?php foreach ($allowedKinds as $kind): ?>
                     <option value="<?= htmlspecialchars($kind, ENT_QUOTES) ?>" <?= $kind === $field['value_kind'] ? 'selected' : '' ?> >
                         <?= htmlspecialchars($kind, ENT_QUOTES) ?>
@@ -23,11 +22,6 @@ $labelTitle = $exercise['title'];
             </select>
         </div>
         <div class="actions">
-            <input class="button" type="submit" name="commit" value="Update Field" data-disable-with="Update Field">
+            <input class="btn btn-purple" type="submit" name="commit" value="Update Field" data-disable-with="Update Field">
         </div>
     </form>
-</div>
-
-
-
-
