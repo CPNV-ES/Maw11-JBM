83eef7144b53ebd242dd01de4ae8e631a3db3cef
bheutschi <bheutschi@gmail.com>
2025-12-23 12:54:05 +0100

    fix: enhance layout and styling of the answer form for improved usability

--- app/View/results/create.php
+++ app/View/results/create.php
@@ -1,17 +1,16 @@
-<?php $title = 'Answering'; ?>
-<div class="container main">
+<?php $title = 'Answering';
+$labelTitle = $exercise['title']; ?>
     <h1>Your take</h1>
-    <p>If you'd like to come back later to finish, simply submit it with blanks</p>
+    <p class="lead">If you'd like to come back later to finish, simply submit it with blanks</p>
     <form action="<?= '/exercises/' . $exercise['id'] . '/results' ?>" accept-charset="UTF-8" method="post">
         <input type="hidden" name="exercises_id" value="<?= $exercise['id'] ?>">
         <?php foreach ($exercise['fields'] as $field): ?>
-        <div class="field">
-            <label for="answer_<?=$field['id']?>"><?= $field['label'] ?></label>
-            <input type="text" name="answer_<?=$field['id']?>" id="answer_<?=$field['id']?>">
+        <div class="field mb-3">
+            <label class="form-label fw-bold" for="answer_<?=$field['id']?>"><?= $field['label'] ?></label>
+            <input class="form-control" type="text" name="answer_<?=$field['id']?>" id="answer_<?=$field['id']?>">
         </div>
         <?php endforeach; ?>
         <div class="actions">
-            <input type="submit">
+            <input class="btn btn-purple" type="submit" name="commit" value="Save" data-disable-with="Save"/>
         </div>
     </form>
-</div>
