8be4a039370ae0bfb577d57ed9f15fd605347e61
bheutschi <bheutschi@gmail.com>
2025-12-23 12:54:14 +0100

    fix: enhance layout and styling of the exercise creation form for improved usability

--- app/View/exercises/create.php
+++ app/View/exercises/create.php
@@ -1,13 +1,11 @@
 <?php $title = 'New Exercise' ?>
-<div class="container main">
     <h1>New Exercise</h1>
     <form action="/exercises" accept-charset="UTF-8" method="post" enctype="multipart/form-data">
-        <div class="field">
-            <label for="exercise_title">Title</label>
-            <input class="input" type="text" name="exercise_title" id="exercise_title" required/>
+        <div class="mb-3">
+            <label for="exercise_title" class="form-label">Title</label>
+            <input class="form-control" type="text" name="exercise_title" id="exercise_title" required/>
         </div>
-        <div class="actions">
-            <input class="button" type="submit" name="commit" value="Create Exercise" data-disable-with="Create Exercise"/>
+        <div class="actions mt-2">
+            <input class="btn btn-purple" type="submit" name="commit" value="Create Exercise" data-disable-with="Create Exercise"/>
         </div>
     </form>
-</div>
